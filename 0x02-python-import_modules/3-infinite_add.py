4493e73ea79be9bb140dd449e3c31513903ff956
Elly <122857130+EljonesA@users.noreply.github.com>
2023-08-10 11:17:29 +0000

    Python import and modules

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+import sys
+
+def cal():
+    if len(sys.argv) <= 1:
+        print("0")
+    else:
+        result = 0
+        for args in sys.argv[1:]:
+            num = int(args)
+            result = result + num
+        print(result)
+
+
+if __name__ == "__main__":
+    cal()
