1707a821bad09441e8150388b625568099814a4e
Elly <122857130+EljonesA@users.noreply.github.com>
2023-09-11 14:41:22 +0000

    Document lookup function

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,5 +1,17 @@
 #!/usr/bin/python3
+"""lookup function"""
 
 
 def lookup(obj):
-    return dir(obj)
+    """
+    Retrieves a list of attributes and methods of an object
+
+    Args:
+        obj (object): the object to inspect
+
+    Returns:
+        list: list of attributes and methods.
+    """
+    attributes_methods = dir(obj)
+
+    return attributes_methods
