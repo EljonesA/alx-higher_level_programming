224b13f78ae4a376d2db4a833fe40c1b466b622d
Elly <122857130+EljonesA@users.noreply.github.com>
2023-09-12 18:18:00 +0000

    Document read_file() function

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -2,6 +2,12 @@
 
 
 def read_file(filename=""):
+    """
+    function that reads and prints data of a file
+
+    Args:
+        filename: the file to read from
+    """
     with open(filename, encoding="utf-8") as f:
         read_data = f.read()
         print(read_data, end="")
