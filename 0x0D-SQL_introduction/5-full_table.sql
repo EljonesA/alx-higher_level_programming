8eb64c04f92f6e29dd1ded45d02d55ac00467d98
Elly <122857130+EljonesA@users.noreply.github.com>
2023-10-19 15:29:59 +0000

    Print full description of a table

---
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -0,0 +1,4 @@
+-- script to print full description of table first_table from db hbtn_0c_0
+-- database name passed as argument of the sql command
+-- DESCRIBE/EXPLAIN statements not allowed
+SHOW CREATE TABLE first_table;
