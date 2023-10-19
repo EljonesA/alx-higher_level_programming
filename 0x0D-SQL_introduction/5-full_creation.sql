b4a83a5622861e60b5de709680599124b0135873
Elly <122857130+EljonesA@users.noreply.github.com>
2023-10-19 16:10:14 +0000

    Create table, columns & Insert

---
+++ 0x0D-SQL_introduction/5-full_creation.sql
@@ -0,0 +1,14 @@
+-- script to create table 'second_table' if doesn't exist
+-- rows: id INT, name varchar(256), score INT
+-- SELECT/SHOWS statements not allowed
+CREATE TABLE IF NOT EXISTS second_table (
+	id INT,
+	name VARCHAR(256),
+	score INT
+);
+
+INSERT INTO second_table (id, name, score) VALUES (1, 'John', 10);
+INSERT INTO second_table (id, name, score) VALUES (2, 'Alex', 3);
+INSERT INTO second_table (id, name, score) VALUES (3, 'Bob', 14);
+INSERT INTO second_table (id, name, score) VALUES (4, 'George', 8);
+
