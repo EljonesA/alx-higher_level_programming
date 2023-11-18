3474124259873fc5cc4de49fa08821e5d6927d37
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 13:05:52 +0000

    Count command-line arguments

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,10 @@
+#!/usr/bin/node
+const myArgs = process.argv.length; // check number of command-line args
+
+if (myArgs === 2) {
+  console.log('No argument');
+} else if (myArgs == 3) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
