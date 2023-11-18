bfe12d98b19cc0f57c8e0eaeebadec95b07802f8
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 15:32:38 +0000

    Search for the second biggest integer in the list of arguments.

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// capture command line args
+const args = process.argv.slice(2);
+
+// check no. of args
+if (args.length <= 1) {
+  console.log(0);
+} else {
+  // convert args to ints then sort them(ascending order)
+  const sortedInts = args.map(Number).sort(a, b) => b - a);
+
+  console.log(sortedInts[1]);
+}
