4685889179dc1598de8e44a678c3e89b71d36682
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 13:49:15 +0000

    Check if an arg is a number

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -2,7 +2,7 @@
 const firstArg = process.argv[2]; // capture first arg
 
 if (!firstArg) {
-  console.log('No argument')
+  console.log('No argument');
 } else {
   console.log(firstArg);
 }
---
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+// capture first argument
+const firstArg = process.argv[2];
+
+// convert the arg to a valid integer
+const convertedInt = parseInt(firstArg);
+
+// check conversion. If successful log the number
+if (!isNan(convertedInt) {
+  console.log('My number: ${convertedInt}');
+} else {
+  console.log('Not a number');
+}
