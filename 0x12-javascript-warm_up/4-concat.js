3f5e471df15e8058cbc08581300527d5ad56345c
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 13:25:35 +0000

    String concatenation

---
+++ 0x12-javascript-warm_up/4-concat.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// Capture first and second command-line args
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+//concatenate the strings
+concatString = firstArg + ' is ' + secondArg;
+
+// print the result
+console.log(concatString);
