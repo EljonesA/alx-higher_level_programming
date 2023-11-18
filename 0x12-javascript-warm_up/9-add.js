ed0688abead695e3ba2fc52019ad64bbdef625ad
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 14:58:00 +0000

    Add two numbers provided as command line arguments

---
+++ 0x12-javascript-warm_up/9-add.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+
+// add function
+function add(a, b) {
+  return a + b;
+}
+
+// capture first argument
+const firstArg = process.argv[2];
+const secondArg = process.argv[3];
+
+// convert the arg to a valid integer
+const convertedFirstArg = parseInt(firstArg);
+const convertedSecondArg = parseInt(secondArg);
+
+// check conversion. If successful add the numbers
+if (!isNaN(convertedFirstArg) && !isNaN(convertedSecondArg)) {
+  console.log(add(convertedFirstArg, convertedSecondArg));
+} else {
+  console.log('NaN');
+}
