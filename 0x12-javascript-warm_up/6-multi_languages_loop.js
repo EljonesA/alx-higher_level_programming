5d33ce062da51b4cf0f3f9ae0d558718f616f5a0
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 14:14:17 +0000

    Loop through an array using a while loop

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+let myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+// loop the array and print each string in the array
+let i = 0;
+while (i < myArray.length) {
+  console.log(myArray[i]);
+  i++;
+}
