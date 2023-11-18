96de1fc1ba8e1f94f8cf42676a4080b174875a3a
Eljones <122857130+EljonesA@users.noreply.github.com>
2023-11-18 15:18:11 +0000

    Compute factorial of a number

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+
+// function to calculate the factorial
+function factorial(n) {
+  if (isNan(n) || n < 0) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+// capture the first argument
+const input = parseInt(process.argv[2]);
+
+//compute & print the factorial
+console.log(factorial (input));
