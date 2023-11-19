885c9bc9ae96f3b73ebc9af4f240b4c196737fa7
Elly <122857130+EljonesA@users.noreply.github.com>
2023-11-19 03:56:09 -0800

    Update States fetch file

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,29 @@
 #!/usr/bin/python3
+"""
+Script to list al states from the database hbtn_0e_0_usa.
+
+Usage:
+    ./0-select_states.py username password database
+"""
+
 # import required modules
 import MySQLdb
 import sys  # capture command line args
 
-if __name__ == "__main__":
-    # ensure correct number of args provided
-    if len(sys.argv) != 4:
-        print("Usage: {} username password database".format(sys.argv[0]))
-        sys.exit(1)
 
-    # capture the command line arguments
-    username, password, database = sys.argv[1:]
+def display_states(username, password, database):
+    """
+    Connects to the MySQL server and retrieves and prints all states
+    from the 'states' table sorted by states.id.
+
+    Args:
+        username (str): MySQL username.
+        password (str): MySQL password.
+        database (str): Database name.
+
+    Returns:
+        None
+    """
 
     # create a connection
     db = MySQLdb.connect(host="localhost", port=3306, user=username,
@@ -28,3 +41,19 @@ if __name__ == "__main__":
     # close the cursor & connection
     cursor.close()
     db.close()
+
+
+if __name__ == "__main__":
+    """
+    Entry point of the script
+    """
+    # ensure correct number of args provided
+    if len(sys.argv) != 4:
+        print("Usage: {} username password database".format(sys.argv[0]))
+        sys.exit(1)
+
+    # capture the command line arguments
+    username, password, database = sys.argv[1:]
+
+    # call display_state function
+    display_states(username, password, database)
