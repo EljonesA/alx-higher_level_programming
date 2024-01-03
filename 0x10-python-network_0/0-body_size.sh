25e4acb20f02a628c5308b0ac1e645fa06d783de
Eljones <122857130+EljonesA@users.noreply.github.com>
2024-01-03 02:21:10 +0000

    Ensure file 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # script that takes in a URL, sends request to that URL, returns response size
-response_size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}')
+curl -sI "$1" | grep -i content-length | awk '{print $2}'
