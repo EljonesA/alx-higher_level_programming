210d8e5f825ea79a9dc9121d9d2450e617593943
Eljones <122857130+EljonesA@users.noreply.github.com>
2024-01-03 02:17:42 +0000

    Ensure file 3 lines

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,9 +1,3 @@
 #!/bin/bash
-# script that takes in a URL, sends a request to that URL,
-# and displays the size of the body of the response
-
-# send request(I option to return only header)
-# grep to search for string content-length in response header
-# awk to extract 2nd arg from filtered line content-length
-url="$1"
-response_size=$(curl -sI "$url" | grep -i content-length | awk '{print $2}')
+# script that takes in a URL, sends request to that URL, returns response size
+response_size=$(curl -sI "$1" | grep -i content-length | awk '{print $2}')
