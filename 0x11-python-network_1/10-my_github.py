0372b250d34dd9f3c1bf2fb9018429d3e7685ec1
Eljones <122857130+EljonesA@users.noreply.github.com>
2024-01-27 12:04:08 +0300

    Github API

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" script that takes your GitHub credentials (username and password) and uses
+    the GitHub API to display your id > using Basic authentication
+"""
+
+import sys
+import requests
+
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = 'https://api.github.com/user'
+
+    headers = {'Authorization': 'Basic ' + f'{username}:{password}'}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        # Parse the response as JSON
+        data = response.json()
+        # Display the user id
+        print(data.get('id'))
+    else:
+        print(None)
