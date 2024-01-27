38d75ba2447a7f300c4ae0614637b0cd16f1a1d6
Eljones <122857130+EljonesA@users.noreply.github.com>
2024-01-27 10:28:40 +0300

    Use \t instead of spaces

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
         # read response content
         content = response.read()
         print("Body response:")
-        print(f"    - type: {type(content)}")
-        print(f"    - content: {content}")
-        print(f"    - utf 8 content: {content.decode('utf-8')}")
+        print(f"\t- type: {type(content)}")
+        print(f"\t- content: {content}")
+        print(f"\t- utf 8 content: {content.decode('utf-8')}")
