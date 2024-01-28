89ae104f742beff54396d8e3a37b4151562ca54e
Eljones <122857130+EljonesA@users.noreply.github.com>
2024-01-28 18:24:45 +0300

    Satisfy pycodestyle

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,20 +1,36 @@
 #!/usr/bin/python3
 
+""" Please list 10 commits (from the most recent to oldest) of the repository
+    “rails” by the user “rails”
+    You must use the GitHub API, here is the documentation
+    https://developer.github.com/v3/repos/commits/
+    Print all commits by: `<sha>: <author name>` (one by line)
+"""
+
 import sys
 import requests
 
+
 def fetch_commits(repo_name, owner_name):
+    '''
+        Fetches 10 recent commits of a repository
+        Args:
+            repo_name: the repository to fetch from
+            owner_name: Repository's owner
+    '''
+
     url = f"https://api.github.com/repos/{owner_name}/{repo_name}/commits"
     response = requests.get(url)
 
     if response.status_code == 200:
-        commits = response.json()[:10] # 10 recent commits
+        commits = response.json()[:10]  # 10 recent commits
         for commit in commits:
             sha = commit['sha']
             author_name = commit['commit']['author']['name']
             print(f"{sha}: {author_name}")
     else:
-        print(f"Error: Failed to fetch commits. Status code: {response.status_code}")
+        print(f"Error: Failed to fetch commits. Status code:\
+              {response.status_code}")
 
 
 if __name__ == "__main__":
