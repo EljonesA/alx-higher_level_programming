2637a3f82269fcd7c8647bb64bc132226392148c
Eljones <eljones.odongo@gmail.com>
2024-02-20 17:01:31 +0300

    Fetch contents of a webpage and stores it in a file.

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+// script that gets the contents of a webpage and stores it in a file.
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request.get(url, (error, response, body) => {
+  if (error) {
+    process.exit(1);
+  }
+  //  write to file
+  fs.writeFile(filePath, body, 'utf-8', (err) => {
+    if (err) {
+      console.log(err);
+      process.exit(1);
+    }
+  });
+});
